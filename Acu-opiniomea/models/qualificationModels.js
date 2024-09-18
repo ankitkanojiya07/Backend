3f3f5dfa0f79eccaf7e9965b11d52273d94389fa
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-09-18 15:03:41 +0530

    Update qualificationModels.js

--- Acu-opiniomea/models/qualificationModels.js
+++ Acu-opiniomea/models/qualificationModels.js
@@ -1,26 +1,35 @@
-const {DataTypes} = require('sequelize');
-const sequelize = route('./config');
+// Import the necessary Sequelize components
+const { DataTypes } = require('sequelize');
+// Import the Sequelize instance from the configuration file
+const sequelize = require('./config');  // Corrected 'route' to 'require'
 
-const Qualification = sequelize.define('Qualification', {
-    Age : {
-        type : DataTypes.INTEGER
+// Define the Qualification model with Sequelize
+const Qualification = sequelize.define('Qualification', {  // Model name is 'Qualification'
+    Age: {
+        type: DataTypes.INTEGER,    // Data type for the 'Age' field
+        allowNull: true,            // Allows the field to be null (optional)
     },
-    Education : {
-        type : DataTypes.STRING
+    Education: {
+        type: DataTypes.STRING,     // Data type for the 'Education' field
+        allowNull: true,            // Allows the field to be null (optional)
     },
-    Experience : {
-        type : DataTypes.STRING
+    Experience: {
+        type: DataTypes.STRING,     // Data type for the 'Experience' field
+        allowNull: true,            // Allows the field to be null (optional)
     },
-    Gender : {
-        type : DataTypes.STRING
+    Gender: {
+        type: DataTypes.STRING,     // Data type for the 'Gender' field
+        allowNull: true,            // Allows the field to be null (optional)
     },
-    Country : {
-        type : DataTypes.STRING
+    Country: {
+        type: DataTypes.STRING,     // Data type for the 'Country' field
+        allowNull: true,            // Allows the field to be null (optional)
     },
-    State : {
-        type : DataTypes.STRING
+    State: {
+        type: DataTypes.STRING,     // Data type for the 'State' field
+        allowNull: true,            // Allows the field to be null (optional)
     }
-})
+});
 
-
+// Export the Qualification model for use in other parts of the application
 module.exports = Qualification;

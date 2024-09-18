f7d019af2ceb79f232f004f533708b4ea5ed1e55
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-09-18 05:38:28 +0000

    first

--- Acu-opiniomea/models/surveyModels.js
+++ Acu-opiniomea/models/surveyModels.js
@@ -6,34 +6,91 @@ const Survey = sequelize.define('Survey', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+
+    AccountID: {
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
+        allowNull: false,
+    },
     
-    SurveyName: {
+    SurveyStatusCode: {
         type: DataTypes.STRING,
+        allowNull: true,
     },
+    
+    CountryLanguageID: {
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
+        allowNull: true,
+    },
+    
+    IndustryID: {
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
+        allowNull: false,
+    },
+    
+    StudyTypeID: {
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
+        allowNull: false,
+    },
+
+    ClientCPI: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+    },
+  
+    ClientSurveyLiveURL: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+ 
+    TestRedirectURL: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    IsActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+
+    Quota: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+
+    SurveyName: {
+        type: DataTypes.STRING, // Removed duplicate
+    },
+
     Completes: {
-        type: DataTypes.INTEGER, // Use INTEGER instead of NUMBER
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
     },
+
     createdAt: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
     },
+
     updatedAt: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
     },
+
     status: {
-        type: DataTypes.STRING, // Use INTEGER instead of NUMBER
+        type: DataTypes.STRING, // Changed to DataTypes.STRING
     },
+
     id: {
-        type: DataTypes.INTEGER, // Use INTEGER instead of NUMBER
+        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
         primaryKey: true,
-        autoIncrement: true, // Add autoIncrement
+        autoIncrement: true, // Added autoIncrement
     },
+
     endedAt: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
     },
+
     country: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
     },
 }, {
     timestamps: true,

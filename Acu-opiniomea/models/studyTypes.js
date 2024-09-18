9b87b4ee79982193e9fc4802ffc1e0fba54d0b0c
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-09-18 15:10:58 +0530

    Update studyTypes.js

--- Acu-opiniomea/models/studyTypes.js
+++ Acu-opiniomea/models/studyTypes.js
@@ -1,22 +1,28 @@
+// Import the necessary Sequelize components
 const { DataTypes } = require('sequelize');
+// Import the Sequelize instance from the configuration file
 const sequelize = require('../config'); 
 
-const studyTypes = sequelize.define('Survey', {
+// Define the 'studyTypes' model with Sequelize
+const studyTypes = sequelize.define('StudyTypes', {  // Changed model name to 'StudyTypes'
     Code: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING,  // Data type for the 'Code' field
+        allowNull: false,        // 'Code' cannot be null
     },
 
     ID: {
-        type: DataTypes.INTEGER, // Changed to DataTypes.INTEGER
-        allowNull: false,
+        type: DataTypes.INTEGER, // Data type for the 'ID' field
+        allowNull: false,        // 'ID' cannot be null
+        autoIncrement: true,     // Automatically increment the ID value
+        primaryKey: true,        // 'ID' is the primary key of the table
     },
     
     Name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING,  // Data type for the 'Name' field
+        allowNull: false,        // 'Name' cannot be null
     },
     
 });
 
+// Export the 'studyTypes' model for use in other parts of the application
 module.exports = studyTypes;

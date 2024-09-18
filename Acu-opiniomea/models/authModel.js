3bb6ddc5d09febd02a19625b902afdc8c73259c8
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-09-18 15:02:14 +0530

    Update authModel.js

--- Acu-opiniomea/models/authModel.js
+++ Acu-opiniomea/models/authModel.js
@@ -1,18 +1,19 @@
+// Import the necessary Sequelize components
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config'); 
+const sequelize = require('../config');
 
-const AuthModel = sequelize.define('Survey', {
+// Define the AuthModel with Sequelize
+const AuthModel = sequelize.define('Survey', {  // Model name 'Survey' might be misleading for this model
     AccountID: {
-        type: DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
+        type: DataTypes.INTEGER,       // Data type for the 'AccountID' field
+        primaryKey: true,              // Marks this field as the primary key of the model
+        autoIncrement: true,           // Automatically increment this field's value
     },
-    
     ApiKey: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING,        // Data type for the 'ApiKey' field
+        allowNull: false,              // 'ApiKey' field must have a value (cannot be null)
     },
-    
 });
 
+// Export the AuthModel for use in other parts of the application
 module.exports = AuthModel;

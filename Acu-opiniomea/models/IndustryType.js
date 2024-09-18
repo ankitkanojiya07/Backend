3e5462d4851676c29cbd6d6958f8c4fc68ab6078
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-09-18 15:01:30 +0530

    Update IndustryType.js

--- Acu-opiniomea/models/IndustryType.js
+++ Acu-opiniomea/models/IndustryType.js
@@ -1,16 +1,18 @@
+// Import the necessary Sequelize components
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config'); 
+const sequelize = require('../config');
 
-const IndustryType = sequelize.define('Survey', {
+// Define the IndustryType model with Sequelize
+const IndustryType = sequelize.define('Survey', {  // Model name 'Survey' might be confusing for this model
     Code: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING,  // Data type for the 'Code' field
+        allowNull: false,        // 'Code' field must have a value (cannot be null)
     },
     Name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING,  // Data type for the 'Name' field
+        allowNull: false,        // 'Name' field must have a value (cannot be null)
     },
-    
 });
 
+// Export the IndustryType model for use in other parts of the application
 module.exports = IndustryType;
